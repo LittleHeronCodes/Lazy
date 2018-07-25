3ea463805c3c62d9c84a154d0107ab22ab42ce65
Yeogha Yoon <25128289+StardustJinn@users.noreply.github.com>
2018-07-25 18:42:21 +0900

    Update perconn.R

--- custom/perconn.R
+++ custom/perconn.R
@@ -1,12 +1,12 @@
 # Database connection function (server)
 
 # connection function
-perconn <- function(db = "CDRgator", server = TRUE, usern = "yeogha", pw = "123456") {
+perconn <- function(db = "", server = TRUE, usern = "", pw = "") {
 	require(RMySQL)
 	if(server) {
 		conn <- dbConnect(dbDriver("MySQL"), host = "203.255.191.201", user = usern, password = pw, dbname = db, port = 3307)
 	} else{
-		conn <- dbConnect(dbDriver("MySQL"), user='jinn', password='airpede14', dbname=db)
+		conn <- dbConnect(dbDriver("MySQL"), user='', password='', dbname=db)
 	}
 	return(conn)
 		}
