93bd8035a0af25a462247be8b43ee5c72359a02a
Yeogha <25128289+StardustJinn@users.noreply.github.com>
2018-08-28 10:56:21 +0900

    Update R_package_download.R

--- custom/R_package_download.R
+++ custom/R_package_download.R
@@ -1,24 +1,43 @@
+## important packages in case of new installations
 
-source('https://bioconductor.org/biocLite.R')
-
+# install via cran
 install.packages('BH')
-install.packages('dplyr')
-install.packages('reshape')
 install.packages('RMySQL')
 install.packages('RColorBrewer')
 install.packages('ggplot2')
+install.packages('data.table')
+install.packages('devtools')
+install.packages('vennDiagram')
+
+# tidyverse series
+# install.packages('tidyverse')
+install.packages('dplyr')
+install.packages('reshape')
+install.packages('magrittr')
+install.packages('readxl')
+
+
+# install via bioconductor
+source('https://bioconductor.org/biocLite.R')
 
 biocLite()
-biocLite('AnnotationDbi')
-biocLite('DBI')
+biocLite('BioInstaller')
 biocLite('Biobase')
 biocLite(c('Biostrings', 'BiocGenerics', 'BiocParallel'))
+biocLite('DBI')
+biocLite('AnnotationDbi')
 biocLite('affy')
 biocLite('gcrma')
 biocLite('genefilter')
 biocLite('simpleaffy')
-biocLite(c('hgu133plus2cdf', 'hgu133acdf', 'hgu133a2cdf','hugene10stv1cdf'))
 biocLite('limma')
+biocLite(c('hgu133plus2cdf', 'hgu133acdf', 'hgu133a2cdf','hugene10stv1cdf'))
+biocLite('DESeq2')
+
+biocLite('ComplexHeatmap')
+biocLite(c("RBGL","graph")) # for Vennerable package
 
 
-biocLite('BioInstaller')
+# install via devtools
+library(devtools)
+install_github("js229/Vennerable")
